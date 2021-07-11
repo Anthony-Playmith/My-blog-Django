531722df2a2217976706247a314a8134d50946b3
AnthonyPlaymith <anthony.playmith@schoolmint.net>
2021-07-10 23:58:10 -0600

    added new files

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def starting_page(request):
+    '''Function for the starting page'''
+    return render(request, "blog/index.html")
+
+def posts(request):
+    '''Required for the post routes/URLs'''
+    pass
+
+def post_detail(request):
+    '''
+    Function for the individual post. It should be trigger by our last path in urls.py
+    '''
+    pass
+
